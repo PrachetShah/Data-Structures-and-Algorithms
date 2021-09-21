527eb1b64480d605570cdc19be58643c8e95902a
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2021-09-21 17:52:53 +0530

    Create ReverseAString.js

---
+++ Section 3 Arrays/Exercises/ReverseAString.js
@@ -0,0 +1,25 @@
+//Function to reverse a string
+
+function reverse(str){
+  if(!str || typeof str != 'string' || str.length < 2 ) return str;
+
+  let name = str.split('');
+  let rev = [];
+  for(let i=name.length-1; i>=0; i--){
+    rev.push(name[i]);
+  }
+  return rev.join('');
+}
+
+str = 'My name is Prachet';
+reverse(str);
+
+/* 
+Other Methods:
+function reverse2(str){
+  //check for valid input in both
+  return str.split('').reverse().join('');
+}
+
+const reverse3 = str => [...str].reverse().join('');
+*/
